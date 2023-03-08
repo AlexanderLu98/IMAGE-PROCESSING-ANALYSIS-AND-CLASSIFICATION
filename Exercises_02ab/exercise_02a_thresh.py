96496dde0693af6ecb7b63d1838a8b8c91d65209
Alexander Lu <alex.lu2710@gmail.com>
2023-03-08 16:08:20 +0100

    Fixed 02a for terminal

--- Exercises_02ab/exercise_02a_thresh.py
+++ Exercises_02ab/exercise_02a_thresh.py
@@ -23,13 +23,11 @@ if len(sys.argv) != 4:
     sys.exit(1)
 else:
     try:
-        img1 = cv2.imread("Exercises_02ab\exercise_02a_input_01.pgm", cv2.IMREAD_GRAYSCALE)
+        img1 = cv2.imread(sys.argv[1], cv2.IMREAD_GRAYSCALE)
+        value = int(sys.argv[2])
         row = img1.shape[0]
         column = img1.shape[1]
         img2 = img1.copy()
-
-        #Gives the oppurtunity to choose the value
-        value = int(input("Input value as an Integer: "))
 
         for i in range(row):
             for j in range(column):
@@ -38,14 +36,13 @@ else:
                     img2[int(i), int(j)] = 255
                 else:
                     img2[int(i), int(j)] = value
-    
-        #Saving the picture file, can also implement the decesion to input the path to the chosen file
-        print("Saving image to file exercise_02a_output_01.pgm ...")
+
+        print("Saving image to file in folder")
         #The file called "Exercises_02ab\exercise_02a_output_01.pgm" that is in this folder is created by this code with a value at 100
-        cv2.imwrite("Exercises_02ab\exercise_02a_output_01.pgm", img2)
+        cv2.imwrite(sys.argv[3], img2)
 
         cv2.imshow("Window: img2", img2)
-        cv2.waitKey(0) # waiting for a key
+        cv2.waitKey(0)
 
     except Exception as e:
         print(e)
